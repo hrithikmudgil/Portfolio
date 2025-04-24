caa80a5b59d3bd677e787c1f24c1be66ace44539
hrithik <hrithikmudgil.com>
2025-04-24 14:59:26 +0530

    wgr

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PortfolioBuild/',  // <-- Correct this to your GitHub repo name
+  base: '/Portfolio/',  // <-- Correct this to your GitHub repo name
 });
